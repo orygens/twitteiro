71c2b859ab875c19a3ae08b58a824e929a363f1e
rodrigo3n <rodrigo3n@gmail.com>
2010-08-07 12:18:43 -0500

    Adicionando suporte default ao português, melhorando documentação

--- config/application.rb
+++ config/application.rb
@@ -5,24 +5,15 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Twitteiro
   class Application < Rails::Application
-    # :all can be used as a placeholder for all plugins not explicitly named
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
     config.time_zone = 'America/Recife'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :pt_BR
+    config.encoding = 'utf-8'
+    config.filter_parameters += [:password]
 
     config.generators do |g|
       g.template_engine  :erb
       g.test_framework   :test_unit, :fixture => true
       g.integration_tool :rspec
     end
-    config.encoding = "utf-8"
-    config.filter_parameters += [:password]
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,11 @@ Twitteiro::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # Specifies the header that your server uses for sending files. Apache mode
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
-  # If you have no front-end server that supports something like X-Sendfile,
-  # just comment this out and Rails will serve the files
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -31,10 +28,6 @@ Twitteiro::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable threaded mode
-  # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
+  config.threadsafe!
   config.i18n.fallbacks = true
 end
