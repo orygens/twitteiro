6055d2a6049744784604221305e61ea63d8f050c
rodrigo3n <rodrigo3n@gmail.com>
2010-08-12 21:33:06 -0500

    Simplificando o OAuth controller, parei na fase do PIN :-(. Tenho que terminar isso de uma vez amanhã no máximo

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,30 +1,20 @@
-# -*- encoding: utf-8 -*-
 class OauthController < ApplicationController
   def start
     @request_token = client.get_request_token
-    session[:request_token] = @request_token.token
-    session[:request_token_secret] = @request_token.secret
+    session[:request_token] = @request_token
+    session[:request_token_secret] = @request_token
     redirect_to @request_token.authorize_url
   end
   
   def callback
     @request_token = client.get_request_token(:oauth_callback => 'http://localhost:3000')
     @access_token = @request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-    @response = client.request(:get, 'https://api.twitter.com/account/verify_credentials.json', @access_token, { :scheme => :query_string })
-    case @reponse
-    when Net::HTTPSuccess
-      user_info = JSON.parse(@response.body)
-      unless user_info['screen_name']
-        flash[:error] = "Algo deu errado com a autenticação do Twitter"
-        redirect_to root_path
-      end
-    end
   end
 
   private 
 
   def client
-    @consumer = OAuth::Consumer.new(
+    @client = OAuth::Consumer.new(
       'bHU901qt0sXxPslj6nGukQ','MM1E9xzbz5YvC7OrlZwV80eQEYPjCWeF6t5xE2sCprk',
       :site => 'https://api.twitter.com',
       :authorize_url => 'https://api.twitter.com/oauth/authorize',
