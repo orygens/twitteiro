636453db65f860c374e6fda07555a62f5387cd7c
Rodrigo Alves Vieira <rodrigo3n@gmail.com>
2010-09-23 19:18:40 -0300

    Adicionando validação no model dos apoios e melhorando views

--- app/models/supportship.rb
+++ app/models/supportship.rb
@@ -6,5 +6,10 @@ class Supportship < ActiveRecord::Base
   def supporter=(id)
     supporter_id = id
   end
+ 
+  #validates_inclusion_of :frequency, :in => %w(4 8 12 24 48)
   
+  def verify_uniqueness
+    ! Supportship.where(:user_id => user_id).and(:supporter_id => :supporter_id).empty?
+  end
 end
